65c1d18dd7be0d19225ac984f83ee4a58a4ac22f
SunyPiggy <32224398+SunyPiggy@users.noreply.github.com>
2018-06-20 11:02:01 +0800

    Initial commit

---
+++ application/route.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\Route;
+use think\Url;
+Url::root('/');
+Route::group('api',function (){
+    Route::group('v1',function (){
+        Route::bind('index');
+        Route::resource('cat','Index');
+
+
+
+
+
+
+
+
+    });
+    Route::group('v2',function (){
+
+
+
+
+    });
+    Route::miss(function (){
+        echo 133;
+    });
+});
+
